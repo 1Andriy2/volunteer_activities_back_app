ff53190f153828144c5c9c88f74ce3b6737a1926
Andriy Kostelniy <andrijkostelnij72@gmail.com>
2024-12-04 23:17:31 +0200

    fix

--- src/api/activity/content-types/activity/lifecycles.js
+++ src/api/activity/content-types/activity/lifecycles.js
@@ -1,42 +1,42 @@
-module.exports = {
-    async afterCreate(event) {
-      const activity = event.result;
+// module.exports = {
+//     async afterCreate(event) {
+//       const activity = event.result;
   
-      // Отримайте користувачів, які мають отримати сповіщення
-      const users = await strapi.db.query('api::user.user').findMany({
-        where: { id: activity.user.id },
-      });
+//       // Отримайте користувачів, які мають отримати сповіщення
+//       const users = await strapi.db.query('api::user.user').findMany({
+//         where: { id: activity.user.id },
+//       });
   
-      const tokens = users.map(user => user.pushToken).filter(Boolean);
+//       const tokens = users.map(user => user.pushToken).filter(Boolean);
   
-      if (tokens.length > 0) {
-        await strapi.services.notification.sendExpoPushNotification(
-          tokens,
-          'Нова подія!',
-          `Подія "${activity.title}" була створена!`,
-          { eventId: activity.id }
-        );
-      }
-    },
+//       if (tokens.length > 0) {
+//         await strapi.services.notification.sendExpoPushNotification(
+//           tokens,
+//           'Нова подія!',
+//           `Подія "${activity.title}" була створена!`,
+//           { eventId: activity.id }
+//         );
+//       }
+//     },
   
-    async afterUpdate(event) {
-      const activity = event.result;
+//     async afterUpdate(event) {
+//       const activity = event.result;
   
-      // Подібна логіка для оновлення подій
-      const users = await strapi.db.query('api::user.user').findMany({
-        where: { id: activity.user.id },
-      });
+//       // Подібна логіка для оновлення подій
+//       const users = await strapi.db.query('api::user.user').findMany({
+//         where: { id: activity.user.id },
+//       });
   
-      const tokens = users.map(user => user.pushToken).filter(Boolean);
+//       const tokens = users.map(user => user.pushToken).filter(Boolean);
   
-      if (tokens.length > 0) {
-        await strapi.services.notification.sendExpoPushNotification(
-          tokens,
-          'Оновлення події!',
-          `Подію "${activity.title}" було оновлено.`,
-          { eventId: activity.id }
-        );
-      }
-    },
-  };
+//       if (tokens.length > 0) {
+//         await strapi.services.notification.sendExpoPushNotification(
+//           tokens,
+//           'Оновлення події!',
+//           `Подію "${activity.title}" було оновлено.`,
+//           { eventId: activity.id }
+//         );
+//       }
+//     },
+//   };
   

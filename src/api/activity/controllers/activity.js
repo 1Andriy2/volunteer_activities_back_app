cc2fc04ec61005ce0d335d7c14f10c064d618f0d
Andriy Kostelniy <andrijkostelnij72@gmail.com>
2024-12-17 07:46:16 +0200

    fix

--- src/api/activity/controllers/activity.js
+++ src/api/activity/controllers/activity.js
@@ -1,17 +1,7 @@
 'use strict';
 
-module.exports = {
-    async recommendations(ctx) {
-        try {
-            if (!ctx.state.user) {
-                return ctx.throw(401, 'User not authenticated');
-            }
-    
-            const userId = ctx.state.user.id; // ID поточного користувача
-            const recommendations = await strapi.service('api::activity.activity').getRecommendations(userId);
-            ctx.send(recommendations);
-        } catch (err) {
-            ctx.throw(400, err.message);
-        }
-    },    
-};
+/**
+ * activity controller
+ */
+const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::activity.activity');
--- src/api/activity/routes/activity.js
+++ src/api/activity/routes/activity.js
@@ -1,40 +1,8 @@
 'use strict';
 
+/**
+ * activity router
+ */
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/activities/recommendations',
-        handler: 'activity.recommendations', 
-        config: {
-          policies: [],
-          middlewares: [],
-        },
-      },
-      {
-        method: 'GET',
-        path: '/activities',
-        handler: async (ctx) => {
-          try {
-            const activities = await strapi.entityService.findMany(
-              'api::activity.activity',
-              {
-                populate: '*', 
-              }
-            );
-            ctx.body = activities; 
-          } catch (error) {
-            ctx.status = 500;
-            ctx.body = { error: 'Не вдалося отримати активності', details: error.message };
-          }
-        },
-        config: {
-          policies: [],
-          middlewares: [],
-        },
-      },
-    ],
-  };
-  
+module.exports = createCoreRouter('api::activity.activity');
